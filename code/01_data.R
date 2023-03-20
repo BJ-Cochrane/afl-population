ce19bc7913f5ed06315baad0d7f6796a8aca92e8
BJ-Cochrane <b.cochrane.j@gmail.com>
2023-03-20 23:42:31 +1100

    generated birth and death filtering, first crack at animation of population pyramid

--- code/01_data.R
+++ code/01_data.R
@@ -40,66 +40,118 @@ population_year <-
   select(season,age,n) %>%
   unique()
 
-ggplot(population_year)+
-  geom_col(aes(x=age, y = n), size = 1, alpha = 0.5) +
-  geom_hline(aes(yintercept=mean(age)),size = 1 , colour = "red")+
+player_stats_afltables <- fitzRoy::fetch_player_details_afltables() %>%
+  select(Player,Games,Debut,Last) %>%
+  rename(name = Player) %>%
+  group_by(name) %>%
+  filter(Games == max(Games))
+
+population_merged <-
+  population %>%
+  mutate(name = str_c(firstName,surname,sep = " "))%>%
+  left_join(player_stats_afltables, by = "name") %>%
+  mutate(Debut = gsub('[d|y]','',Debut),
+         Last = gsub('[d|y]','',Last))%>%
+  separate(Debut, c("debut_y","debut_d"))%>%
+  separate(Last, c("last_y","last_d")) %>%
+  mutate(across(starts_with('debut'), ~ as.numeric(.)),
+         across(starts_with('last'), ~ as.numeric(.)),
+         across(ends_with("_d"), ~ ./365)) %>%
+  mutate(debut = debut_y + debut_d,
+         last = last_y + last_d) %>%
+  select(-debut_y,-debut_d,-last_y,-last_d)
+
+population_life <-
+population %>%
+  mutate(name = str_c(firstName,surname,sep = " "))%>%
+  group_by(id)%>%
+  arrange(season)%>%
+  mutate(duplicated = duplicated(id)) %>%
+  mutate(status =
+           case_when(row_number()==1 ~ 'birth',
+                     row_number()==n() & season != "2023" ~ "death",
+                     TRUE ~ 'alive')) %>%
+  mutate(status = case_when(n()==1 & season != "2023"  ~ "birth-death",
+                            TRUE ~ status)) %>%
+  ungroup() %>%
+  group_by(season,age,status) %>%
+  summarise(n = n()) %>%
+  ungroup()%>%
+  complete(season,age,status,fill = list(n = 0)) %>%
+  select(age,season,status,n)%>%
+  unique() %>%
+  filter(season != "2014") %>%
+  ungroup()
+
+
+cbbPalette <- c("#0072B2", "#009E73","#E69F00", "#D55E00")
+
+anim <- ggplot(population_life)+
+  geom_col(aes(x=age, y = n, fill = status)) +
+  scale_fill_manual(values = cbbPalette)+
+  labs(title = 'Year: {frame_time}')+
+  transition_time(season)+
+  ease_aes()
+
+animate(anim, fps = 25, duration = 25)
+
+anim_save('test.gif')
+
+population_life %>%
+  select(season,age,status)%>%
+  group_by(season)%>%
+  add_count(age)%>%
+  add_count(status) %>%
+  unique() %>%
+ggplot()+
+  geom_col(aes(x=age, y = n, fill = status))+
   labs(title = 'Year: {frame_time}')+
   theme_classic()+
   transition_time(season)+
   ease_aes()
 
 
-
-
-
-population_club <-
+population_selwood <-
 population %>%
-  select(team,age)%>%
-  group_by(team,age)%>%
-  add_count(age) %>%
-  unique() %>%
-  group_by(team)%>%
+  ungroup()%>%
+  mutate(selwood = case_when(firstName == "Joel" & surname == "Selwood" ~ 0,
+                              TRUE ~ 1)) %>%
+
+  group_by(season)%>%
+  add_count(age,selwood)%>%
+  group_by(season)%>%
   complete(age = seq(min(population$age), max(population$age)),fill = list(n = 0)) %>%
-  ungroup()
-
-population_comp <-
-population %>%
-  select(age)%>%
-  group_by(age)%>%
-  add_count(age) %>%
-  mutate(pct_teams = n/18)%>%
+  select(season,age,n,selwood) %>%
   unique() %>%
-  ungroup()
+  mutate(selwood = as.character(selwood)) %>%
+  mutate(selwood = str_replace_na(selwood,"1"))
 
 
-population_ranges <-
-population %>%
-  select(age)%>%
-  group_by(age)%>%
-  add_count(age) %>%
-  unique() %>%
-  mutate(ranges = as.character(cut(age, seq(18,36, by = 2)))) %>%
-  mutate(ranges = str_replace(ranges, ",","-"),
-         ranges = str_remove(ranges,"\\("),
-         ranges = str_remove(ranges,"\\]")) %>%
-  mutate(ranges = case_when(age == 18 ~ "18-20",
-                            age != 18 ~ ranges)) %>%
-  group_by(ranges) %>%
+ggplot(population_selwood, aes(fill=selwood, y=n, x=age)) +
+  geom_bar(position="stack", stat="identity")
+
+
+
+ggplot(population_year)+
+  geom_col(aes(x=age, y = n), size = 1, alpha = 0.5) +
+  geom_line(aes(x=age, y = n), colour = 'red', size = 2)+
+  labs(title = 'Year: {frame_time}')+
+  theme_classic()+
+  transition_time(season)+
+  ease_aes()
+
+population_year %>%
+  group_by(season) %>%
   summarise(n = sum(n)) %>%
-  ungroup()
 
-ggplot(population_ranges)+
-  geom_line(aes(x=ranges, y = n, group = 1), size = 1, alpha = 0.5)+
-  scale_y_continuous(sec.axis = dup_axis())
-
-
-
-
-
-
-
-### Player data load and save
-
+ggplot()+
+  geom_col(aes(x=season, y = n, fill = n)) +
+  labs(title = 'Year: {frame_time}')+
+  theme_classic()+
+  guides(fill = 'none') +
+  transition_time(season)+
+  shadow_mark()
+  ease_aes()
 
 
 
