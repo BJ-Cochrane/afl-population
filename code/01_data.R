55b8068cd91c4b721dfef59ed8a87936736d5553
BJ-Cochrane <b.cochrane.j@gmail.com>
2023-03-22 00:15:58 +1100

    AFL population charts + gifs

--- code/01_data.R
+++ code/01_data.R
@@ -8,6 +8,9 @@ library(patchwork)
 library(lubridate)
 library(gganimate)
 library(transformr)
+library(zoo)
+library(magick)
+library(scales)
 
 
 player_details <- readRDS(file = "data/player_details_2022.rds")
@@ -68,10 +71,10 @@ population %>%
   arrange(season)%>%
   mutate(duplicated = duplicated(id)) %>%
   mutate(status =
-           case_when(row_number()==1 ~ 'birth',
-                     row_number()==n() & season != "2023" ~ "death",
-                     TRUE ~ 'alive')) %>%
-  mutate(status = case_when(n()==1 & season != "2023"  ~ "birth-death",
+           case_when(row_number()==1 ~ 'Birth',
+                     row_number()==n() & season != "2023" ~ "Death",
+                     TRUE ~ 'Alive')) %>%
+  mutate(status = case_when(n()==1 & season != "2023"  ~ "BirthDeath",
                             TRUE ~ status)) %>%
   ungroup() %>%
   group_by(season,age,status) %>%
@@ -84,74 +87,195 @@ population %>%
   ungroup()
 
 
-cbbPalette <- c("#0072B2", "#009E73","#E69F00", "#D55E00")
+fill_colours <- c(
+          Alive = "#0072B2",
+          Birth = "#009E73",
+          BirthDeath = "#E69F00",
+          Death = "#D55E00")
 
-anim <- ggplot(population_life)+
+
+
+ggplot(population_life)+
   geom_col(aes(x=age, y = n, fill = status)) +
-  scale_fill_manual(values = cbbPalette)+
-  labs(title = 'Year: {frame_time}')+
+  scale_fill_manual(values = fill_colours)+
+  labs(title = 'AFL Population - Year: {frame_time}',
+       x = "Age",
+       y = "Population",
+       fill = "Population status")+
+  theme_classic()+
+  theme(legend.position="bottom",
+        axis.text = element_text(size = 11,
+                                 colour = "black"),
+        plot.title  = element_text(size = 14, face = "bold"),
+        title = element_text(size = 12))+
+  scale_y_continuous(expand = c(0,0))+
+  scale_x_continuous(breaks = c(18:40), expand = c(0.025,0))
+
+
+
+
+anim1 <-
+  ggplot(population_life)+
+  geom_col(aes(x=age, y = n, fill = status)) +
+  scale_fill_manual(values = fill_colours)+
+  labs(title = 'AFL Population - Year: {frame_time}',
+       x = "Age",
+       y = "Population",
+       fill = "Population status")+
+  theme_classic()+
+  theme(legend.position="bottom",
+        axis.text = element_text(size = 11,
+                                 colour = "black"),
+        plot.title  = element_text(size = 14, face = "bold"),
+        title = element_text(size = 12))+
+  scale_y_continuous(expand = c(0,0), breaks = breaks_pretty(8))+
+  scale_x_continuous(breaks = c(18:40), expand = c(0.025,0))+
   transition_time(season)+
   ease_aes()
 
-animate(anim, fps = 25, duration = 25)
+
+a1 <- animate(anim1, fps = 35, duration = 15, width = 500, height = 500)
+
+anim_save('output/pop1.gif', animation = a1)
+
+
+rolling_births_deaths <-
+  population_life %>% arrange(season)%>%
+  group_by(season,status)%>%
+  summarise(n = sum(n)) %>%
+  group_by(status) %>%
+  filter(status %in% c("Birth","Death")) %>%
+  mutate(nsum = cumsum(n))
+
+anim2 <-
+ggplot(rolling_births_deaths)+
+  geom_col(aes(x=status,y=nsum,fill = status))+
+  scale_fill_manual(values = fill_colours[c(2,4)])+
+  labs(x = "Population status",
+       y = "Cumulative number")+
+  theme_classic()+
+  theme(legend.position="bottom",
+        axis.text = element_text(size = 11,
+                                 colour = "black"),
+        plot.title  = element_text(size = 14, face = "bold"),
+        title = element_text(size = 12))+
+  scale_y_continuous(expand = c(0,0),
+                     breaks = breaks_pretty(10))+
+  transition_time(season)+
+  ease_aes()
+
+
+
+
+a2 <- animate(anim2, fps = 35, duration = 15,width = 500, height = 500)
+
+anim_save('output/pop2.gif', animation = a2)
+
+
+a_mgif <- image_read(a1)
+b_mgif <- image_read(a2)
+
+new_gif <- image_append(c(a_mgif[1], b_mgif[1]))
+for(i in 1:525){
+  combined <- image_append(c(a_mgif[i], b_mgif[i]))
+  new_gif <- c(new_gif, combined)
+}
+
+new_gif
+
+image_write(new_gif,path = "pop4.gif", format = 'gif')
+
 
 anim_save('test.gif')
 
-population_life %>%
-  select(season,age,status)%>%
+
+rolling_population <-
+  population %>%
+  arrange(season)%>%
   group_by(season)%>%
-  add_count(age)%>%
-  add_count(status) %>%
-  unique() %>%
-ggplot()+
-  geom_col(aes(x=age, y = n, fill = status))+
-  labs(title = 'Year: {frame_time}')+
+  add_count(id)%>%
+  summarise(n = sum(n))
+
+anim3 <-
+ggplot(rolling_population)+
+  geom_line(aes(x=season, y =n), size = 1.5, colour = "#0072B2") +
+  geom_point(aes(x=season, y =n), color = "#D55E00", size = 4)+
+  labs(title = 'AFL population 2014-2023',
+       x = "Season",
+       y = "Population")+
   theme_classic()+
-  transition_time(season)+
+  theme(legend.position="bottom",
+        axis.text = element_text(size = 11,
+                                 colour = "black"),
+        plot.title  = element_text(size = 14, face = "bold"),
+        title = element_text(size = 12))+
+  scale_y_continuous(breaks = breaks_pretty(6))+
+  scale_x_continuous(breaks = c(2014:2023))+
+  transition_reveal(season)+
+  shadow_mark(colour = "#D55E00", size = 4, exclude_layer = 1)+
   ease_aes()
 
+a3 <- animate(anim3, fps = 35, duration = 15,width = 500, height = 500, end_pause = 15)
 
-population_selwood <-
+
+anim_save('output/pop3.gif', animation = a3)
+
+
+
+club_pops <-
 population %>%
-  ungroup()%>%
-  mutate(selwood = case_when(firstName == "Joel" & surname == "Selwood" ~ 0,
-                              TRUE ~ 1)) %>%
+  filter(season == 2023) %>%
+  arrange(season)%>%
+  group_by(season,team)%>%
+  add_count(id) %>%
+  summarise(n = sum(n))%>%
+  select(team,n)
 
-  group_by(season)%>%
-  add_count(age,selwood)%>%
-  group_by(season)%>%
-  complete(age = seq(min(population$age), max(population$age)),fill = list(n = 0)) %>%
-  select(season,age,n,selwood) %>%
-  unique() %>%
-  mutate(selwood = as.character(selwood)) %>%
-  mutate(selwood = str_replace_na(selwood,"1"))
+# Team colours from @crow_data_sci
+team_colours <- c(
+  'Carlton'= '#0e1e2d',
+  'Geelong Cats'= '#1c3c63',
+  'Adelaide Crows'= '#002b5c',
+  'Sydney Swans' = '#ed171f',
+  'St Kilda' = '#ed0f05',
+  'Essendon' = '#cc2031',
+  'Richmond'= '#D8D800',
+  'Melbourne'= '#0f1131',
+  'Hawthorn' = '#4d2004',
+  'Brisbane Lions' = '#a30046',
+  'Gold Coast Suns' = '#d93e39',
+  'Fremantle'= '#2a1a54',
+  'Collingwood'= '#000000',
+  'Port Adelaide'  = '#01b5b6',
+  'Western Bulldogs'  = '#014896',
+  'West Coast Eagles' = '#062ee2',
+  'North Melbourne'= '#013b9f',
+  'GWS Giants' = '#f15c22')
 
-
-ggplot(population_selwood, aes(fill=selwood, y=n, x=age)) +
-  geom_bar(position="stack", stat="identity")
-
-
-
-ggplot(population_year)+
-  geom_col(aes(x=age, y = n), size = 1, alpha = 0.5) +
-  geom_line(aes(x=age, y = n), colour = 'red', size = 2)+
-  labs(title = 'Year: {frame_time}')+
+club_pops %>%
+  mutate(label_y = n - 1) %>%
+  mutate(team = fct_reorder(team, n)) %>%
+  ggplot(aes(y=team,x=n,fill=team))+
+  scale_fill_manual(values=team_colours)+
+  geom_col()+
+  scale_x_continuous(expand = c(0,0),
+                     limits = c(0,50))+
+  labs(
+    x="List size",
+    y="Club",
+    title = "AFL teams list sizes (population)",
+    subtitle = "2023"
+  )+
+  geom_text(aes(x = label_y, label = n),colour = "white", fontface = "bold")+
+  guides(fill = "none")+
   theme_classic()+
-  transition_time(season)+
-  ease_aes()
+  theme(legend.position="bottom",
+        axis.text = element_text(size = 11,
+                                 colour = "black"),
+        plot.title  = element_text(size = 14, face = "bold"),
+        title = element_text(size = 12))
 
-population_year %>%
-  group_by(season) %>%
-  summarise(n = sum(n)) %>%
 
-ggplot()+
-  geom_col(aes(x=season, y = n, fill = n)) +
-  labs(title = 'Year: {frame_time}')+
-  theme_classic()+
-  guides(fill = 'none') +
-  transition_time(season)+
-  shadow_mark()
-  ease_aes()
 
 
 
