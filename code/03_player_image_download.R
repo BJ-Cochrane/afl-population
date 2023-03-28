598f45e3aafa927b2ef8240c8bfedb040f9104a8
BJ-Cochrane <b.cochrane.j@gmail.com>
2023-03-29 00:37:10 +1100

    Generate raster script for stacking AFL player images, bulk exported the top names

--- code/03_player_image_download.R
+++ code/03_player_image_download.R
@@ -1,3 +1,5 @@
+## download player images (to HDD)
+
 library(tidyverse)
 library(purrr)
 library(fitzRoy)
@@ -7,30 +9,12 @@ library(rasterImage)
 library(png)
 library(RCurl)
 library(EBImage)
-library(magick)
 
-library(raster)
-library(rgdal)
-library(rasterVis)
 
 player_details <- readRDS("data/player_details_2022.rds")
 player_stats <- readRDS("data/player_stats_2022.rds")
 
-all_players<-
-player_stats %>%
-  select(season,name,team.name,player.photoURL) %>%
-  unique() %>%
-  mutate(player_id = str_c(season,team.name,name, sep = "_")) %>%
-  unique()
-
-
-download_afl_images <- function(player_id,image_url){
-
-  download.file(image_url,destfile=glue("D:/data/player_photos/{player_id}.png"), mode="wb")
-
-}
-
-walk2(all_players$player_id, all_players$player.photoURL, download_afl_images)
+seasons <- c(2013:2023)
 
 
 download_function <- function(season_no){
@@ -60,3 +44,20 @@ download_function <- function(season_no){
 
 
 walk(seasons,download_function)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ code/04_layer_images.R
@@ -0,0 +1,148 @@
+library(tidyverse)
+library(purrr)
+library(fitzRoy)
+library(glue)
+library(lubridate)
+library(rasterImage)
+library(png)
+library(magick)
+
+
+player_details <- readRDS("data/player_details_2022.rds")
+player_stats <- readRDS("data/player_stats_2022.rds")
+
+team_names <-
+  player_stats %>%
+  select(team.name) %>%
+  unique() %>%
+  na.omit() %>%
+  unlist()
+
+load_and_raster <- function(img_url, alpha){
+
+  png_load <- readPNG(source = img_url)
+
+  png_load[,,4] = alpha
+
+  rasterImage(png_load, 0, 0, 1, 1)
+
+}
+
+
+
+raster_teams <- function(team_name,
+                         alpha_amount = 0.05){
+
+imgpaths <- list.files(path="D:/data/player_photos/",
+                  pattern = glue("^2022_{team_name}"), full.names = T)
+
+
+imgpaths_rev <- imgpaths %>% sort(decreasing = TRUE)
+
+png(glue('output/faces/{team_name}.png'), width = 2, height = 2, units = 'in', res = 150)
+par(mai=c(0,0,0,0))
+plot.new()
+
+walk(imgpaths, load_and_raster, alpha = alpha_amount)
+
+dev.off()
+
+png(glue('output/faces/{team_name}_rev.png'), width = 2, height = 2, units = 'in', res = 150)
+par(mai=c(0,0,0,0))
+plot.new()
+
+walk(imgpaths_rev, load_and_raster, alpha = alpha_amount)
+
+dev.off()
+
+
+imgpaths_shuffle <- imgpaths %>%
+  as_tibble() %>%
+  mutate(id = row_number()) %>%
+  sample_n(nrow(.)) %>%
+  select(value)%>%
+  unlist()
+
+png(glue('output/faces/{team_name}_shuffle.png'), width = 2, height = 2, units = 'in', res = 150)
+par(mai=c(0,0,0,0))
+plot.new()
+
+walk(imgpaths_shuffle, load_and_raster, alpha = alpha_amount)
+
+dev.off()
+
+
+}
+
+
+walk(team_names, raster_teams)
+
+
+
+
+### Custom images
+
+
+
+imgpaths_custom <- list.files(path="D:/data/player_photos/",
+                       pattern = glue("(202\\d|Smith)(?:.+)(202\\d|Smith)"), full.names = T)
+
+
+
+imgpaths_custom <- list.files(path="D:/data/player_photos/",
+                              pattern = glue("(2022|De Koning)(?:.+)(2022|De Koning)"), full.names = T)
+
+  imgpaths_shuffle <- imgpaths_custom %>%
+    as_tibble() %>%
+    mutate(id = row_number()) %>%
+    sample_n(nrow(.)) %>%
+    select(value)%>%
+    unlist()
+
+  png(glue('output/faces/custom/DK_shuffle.png'), width = 2, height = 2, units = 'in', res = 150)
+  par(mai=c(0,0,0,0))
+  plot.new()
+
+  walk(imgpaths_shuffle, load_and_raster, alpha = 0.3)
+
+  dev.off()
+
+
+custom_raster <- function(pattern_name, alpha_set = 0.25){
+
+
+  imgpaths_custom <- list.files(path="D:/data/player_photos/",
+                                pattern = glue("(2022|{pattern_name})(?:.+)(2022|{pattern_name})"), full.names = T)
+
+  imgpaths_shuffle <- imgpaths_custom %>%
+    as_tibble() %>%
+    mutate(id = row_number()) %>%
+    sample_n(nrow(.)) %>%
+    select(value)%>%
+    unlist()
+
+  png(glue('output/faces/custom/{pattern_name}.png'), width = 2, height = 2, units = 'in', res = 150)
+  par(mai=c(0,0,0,0))
+  plot.new()
+
+  walk(imgpaths_shuffle, load_and_raster, alpha = alpha_set)
+
+  dev.off()
+
+}
+
+
+walk(bulk,custom_raster)
+
+
+bulk <-
+  player_stats %>%
+    filter(season == 2022)%>%
+    select(player.surname) %>%
+    group_by(player.surname)%>%
+    add_count() %>%
+    unique() %>%
+    arrange(desc(n)) %>%
+    filter(n > 30) %>%
+    select(player.surname) %>%
+  unlist(0)
